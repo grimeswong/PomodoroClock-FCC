f599755dff00ec2451fdc3662c3743bd93417ff1
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-29 00:46:16 +1000

    Define the session and break buttons that can adjust the time range

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/main.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp, faChevronDown, faPlay, faPause, faStop, faSync} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+
+  let [sessionLength, setSessionLength] = useState(25);
+  let [breakLength, setBreakLength] = useState(5);
+
+  const sessionSet = (e) => {
+    console.log(e.target.value);
+    if(e.target.value === "up" && sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+    }
+    if(e.target.value === "down" && sessionLength > 0) {
+      setSessionLength(sessionLength - 1);
+    }
+  }
+
+  const breakSet = (e) => {
+    console.log(e.target.value);
+    if(e.target.value === "up" && breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+    if(e.target.value === "down" && breakLength > 0) {
+      setBreakLength(breakLength - 1);
+    }
+  }
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -12,17 +36,19 @@ function App() {
           <div className="control-wrapper">
             <div id="session-label">Session Length</div>
             <div className="control">
-              <button id="session-decrement"><FontAwesomeIcon icon={faChevronDown} size="lg"/></button>
-              <div id="session-length">25</div>
-              <button id="session-Increment"><FontAwesomeIcon icon={faChevronUp} size="lg" /></button>
+              <button id="session-decrement" onClick={(e) => sessionSet(e)} value="down"><FontAwesomeIcon className="icon" icon={faChevronDown} size="lg" /></button>
+              <div id="session-length">{sessionLength}</div>
+              <React.Fragment>
+              <button id="session-Increment" onClick={(e) => sessionSet(e)} value="up"><FontAwesomeIcon className="icon" icon={faChevronUp} size="lg" /></button>
+              </React.Fragment>
             </div>
           </div>
           <div className="control-wrapper">
             <div id="break-label">Break Length</div>
             <div className="control">
-              <button id="break-decrement"><FontAwesomeIcon icon={faChevronDown} size="lg" /></button>
-              <div id="break-length">5</div>
-              <button id="break-increment"><FontAwesomeIcon icon={faChevronUp}  size="lg" /></button>
+              <button id="break-decrement" onClick={(e) => breakSet(e)} value="down" ><FontAwesomeIcon icon={faChevronDown} size="lg" /></button>
+              <div id="break-length">{breakLength}</div>
+              <button id="break-increment" onClick={(e) => breakSet(e)} value="up" ><FontAwesomeIcon icon={faChevronUp}  size="lg" /></button>
             </div>
           </div>
         </div>
