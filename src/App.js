f5fc206e01318150f0f81bcd97e972aae7ebe8c8
Shadow Grimes <grimeswongatwork@gmail.com>
2020-03-31 22:01:14 +1000

    Convert App from functional to class component

--- src/App.js
+++ src/App.js
@@ -1,87 +1,99 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import './styles/main.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp, faChevronDown, faPlay, faPause, faSync} from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionLength: 25,
+      breakLength: 5,
+      playCountDown: false,
+      timer: 25
+    }
+  }
 
-  let [sessionLength, setSessionLength] = useState(25);
-  let [breakLength, setBreakLength] = useState(5);
-  let [playCountDown, setPlayCountDown] = useState(false);
-  let [timer, setTimer] = useState(25);
-
-  const sessionSet = (e) => {
+  sessionSet = (e) => {
     console.log(e.target.value);
-    if(e.target.value === "up" && sessionLength < 60) {
-      setSessionLength(sessionLength + 1);
-      if(playCountDown === false) {
-        setTimer(sessionLength + 1);
+    if(e.target.value === "up" && this.state.sessionLength < 60) {
+      this.setState({sessionLength: this.state.sessionLength + 1});
+      if(this.state.playCountDown === false) {
+        this.setState({timer: this.state.sessionLength + 1});
       }
     }
-    if(e.target.value === "down" && sessionLength > 0) {
-      setSessionLength(sessionLength - 1);
-      if(playCountDown === false) {
-        setTimer(sessionLength - 1);
+    if(e.target.value === "down" && this.state.sessionLength > 0) {
+      this.setState({sessionLength: this.state.sessionLength - 1});
+      if(this.state.playCountDown === false) {
+        this.setState({timer: this.state.sessionLength - 1});
       }
     }
   }
 
-  const breakSet = (e) => {
+  breakSet = (e) => {
     console.log(e.target.value);
-    if(e.target.value === "up" && breakLength < 60) {
-      setBreakLength(breakLength + 1);
+    if(e.target.value === "up" && this.state.breakLength < 60) {
+      this.setState({breakLength: this.state.breakLength + 1});
     }
-    if(e.target.value === "down" && breakLength > 0) {
-      setBreakLength(breakLength - 1);
+    if(e.target.value === "down" && this.state.breakLength > 0) {
+      this.setState({breakLength: this.state.breakLength - 1});
     }
   }
 
-  const play = () => {
+  play = () => {
     console.log("play");
-    console.log(playCountDown);
-    setPlayCountDown(!playCountDown);
-    if(playCountDown) {
-      setInterval(() => {
-        console.log("Time is ticking")
-      }, 3000)
+    console.log(this.state.playCountDown);
+    this.setState({playCountDown: !this.state.playCountDown});
+    console.log(this.state.playCountDown);
+    if(this.state.playCountDown===true) {
+      this.timeTicking();
+    } else {
+      clearInterval(this.timeTicking());
+      console.log("Time is stopped");
     }
   }
 
+  timeTicking = () => {
+    setInterval(() => {
+      console.log("Time is ticking");
+    }, 2000);
+  }
 
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <h1>Pomodoro Clock</h1>
-        <div className="setting-wrapper">
-          <div className="control-wrapper">
-            <div id="session-label">Session Length</div>
-            <div className="control">
-              <button id="session-decrement" onClick={(e) => sessionSet(e)} value="down"><FontAwesomeIcon className="icon" icon={faChevronDown} size="lg" /></button>
-              <div id="session-length">{sessionLength}</div>
-              <React.Fragment>
-              <button id="session-Increment" onClick={(e) => sessionSet(e)} value="up"><FontAwesomeIcon className="icon" icon={faChevronUp} size="lg" /></button>
-              </React.Fragment>
+  render() {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <h1>Pomodoro Clock</h1>
+          <div className="setting-wrapper">
+            <div className="control-wrapper">
+              <div id="session-label">Session Length</div>
+              <div className="control">
+                <button id="session-decrement" onClick={(e) => this.sessionSet(e)} value="down"><FontAwesomeIcon className="icon" icon={faChevronDown} size="lg" /></button>
+                <div id="session-length">{this.state.sessionLength}</div>
+                <button id="session-Increment" onClick={(e) => this.sessionSet(e)} value="up"><FontAwesomeIcon className="icon" icon={faChevronUp} size="lg" /></button>
+              </div>
+            </div>
+            <div className="control-wrapper">
+              <div id="break-label">Break Length</div>
+              <div className="control">
+                <button id="break-decrement" onClick={(e) => this.breakSet(e)} value="down" ><FontAwesomeIcon icon={faChevronDown} size="lg" /></button>
+                <div id="break-length">{this.state.breakLength}</div>
+                <button id="break-increment" onClick={(e) => this.breakSet(e)} value="up" ><FontAwesomeIcon icon={faChevronUp}  size="lg" /></button>
+              </div>
             </div>
           </div>
-          <div className="control-wrapper">
-            <div id="break-label">Break Length</div>
-            <div className="control">
-              <button id="break-decrement" onClick={(e) => breakSet(e)} value="down" ><FontAwesomeIcon icon={faChevronDown} size="lg" /></button>
-              <div id="break-length">{breakLength}</div>
-              <button id="break-increment" onClick={(e) => breakSet(e)} value="up" ><FontAwesomeIcon icon={faChevronUp}  size="lg" /></button>
-            </div>
-          </div>
-        </div>
 
-        <div className="countdown-wrapper">
-          <div id="timer-label">Session</div>
-          <div id="time-left">{timer}:00</div>
-          <button id="start_stop" onClick={play}><FontAwesomeIcon icon={faPlay} size="lg" /> | <FontAwesomeIcon icon={faPause} size="lg" /></button>
-          <button id="reset"><FontAwesomeIcon icon={faSync} size="lg" /> Reset</button>
+          <div className="timer-wrapper">
+            <div id="timer-label">Session</div>
+            <div id="time-left">{this.state.timer}:00</div>
+            <button id="start_stop" onClick={this.play}><FontAwesomeIcon icon={faPlay} size="lg" /> | <FontAwesomeIcon icon={faPause} size="lg" /></button>
+            <button id="reset"><FontAwesomeIcon icon={faSync} size="lg" /> Reset</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
